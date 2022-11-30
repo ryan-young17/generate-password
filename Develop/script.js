ecc6f04c8139f146c6aacb474f1b0b5eaeb862ce
Ryan Young <ryanbyoung1@gmail.com>
2022-11-29 20:10:54 -0500

    added variables for password options

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,12 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  var isUppercase = false;
+  var isLowercase = false;
+  var isSpecial = false;
+  var isNumber = false;
   
+  return "";
 }
 
 // Write password to the #password input
